80a4f2aa2bbaa8d238e691582bd5f582ccffa2b7
Rahulc15 <rahul.chowdhury9707@gmail.com>
2020-07-19 20:32:31 +0530

    added mock articles

--- ng-blog/src/app/app.module.ts
+++ ng-blog/src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      FooterComponent,
+      NavBarComponent,
+      ArticleListComponent,
+      AboutComponent
+   ],
+   imports: [
+	 BrowserModule,
+	 AppRoutingModule
+	],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
---
+++ ng-blog/src/app/article-list/article-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ARTICLES } from '../mock-articles';
+import { Article } from '../article';
+import { ArticleService } from '../article.service';
+
+@Component({
+  selector: 'app-article-list',
+  templateUrl: './article-list.component.html',
+  styleUrls: ['./article-list.component.css']
+})
+export class ArticleListComponent implements OnInit {
+
+  articles: Article[] = [];
+
+  constructor(private articleService: ArticleService) { }
+
+  ngOnInit() {
+    this.getArticles();
+  }
+
+  getArticles(): void{
+    this.articleService.getArticles().subscribe(articles => this.articles = articles);
+  }
+
+}
