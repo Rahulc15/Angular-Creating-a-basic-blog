39a952e48909b8f0c440b160cb08462d182ef9f6
Rahulc15 <rahul.chowdhury9707@gmail.com>
2020-07-19 20:46:03 +0530

    checking on key parameter of article

--- ng-blog/src/app/app.module.ts
+++ ng-blog/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FooterComponent } from './footer/footer.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { AboutComponent } from './about/about.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
    declarations: [
@@ -14,7 +15,8 @@ import { AboutComponent } from './about/about.component';
       FooterComponent,
       NavBarComponent,
       ArticleListComponent,
-      AboutComponent
+      AboutComponent,
+      ArticleComponent
    ],
    imports: [
 	 BrowserModule,
